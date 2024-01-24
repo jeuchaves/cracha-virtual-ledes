fb4b18bc63d690078b4ba03f9251a9c9c2bb1461
Jeú Chaves <jeuchaveslima@gmail.com>
2024-01-24 19:29:40 -0400

    feat: inicio da página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './pages/Login'
 import User from "./pages/User";
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/user/:id" element={<User/>}/>
-        <Route path="/edituser/:id" element={<EditUser/>}/>
+        <Route path="/edit-user/:id" element={<EditUser/>}/>
         <Route path="/admin" element={<Admin/>}/>
       </Routes>
     </Router>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,35 @@
+import { Card, Form, Button } from 'react-bootstrap'
+
 function Login() {
+
+    const handleLogin = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Lógica de autenticação
+    }
+
     return (
-        <h1>Login</h1>
+        <div className="d-flex align-items-center justify-content-center p-4" style={{ minHeight: '100vh' }}>
+            <Card className="col-12">
+                <Card.Body>
+                    <Card.Title>Login</Card.Title>
+                    <Form onSubmit={handleLogin}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Digite seu email" required />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password" placeholder="Digite sua senha" required />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Entrar
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
