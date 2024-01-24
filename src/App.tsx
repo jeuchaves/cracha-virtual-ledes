f6a3dfecc37bd786ede64599c27d638b5176ade4
Jeú Chaves <jeuchaveslima@gmail.com>
2024-01-24 18:16:44 -0400

    feat: adicionar sistema de roteamento com React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './pages/Login'
+import User from "./pages/User";
+import EditUser from "./pages/EditUser";
+import Admin from "./pages/Admin";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Olá, mundo!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/user/:id" element={<User/>}/>
+        <Route path="/edituser/:id" element={<EditUser/>}/>
+        <Route path="/admin" element={<Admin/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/EditUser.tsx
@@ -0,0 +1,7 @@
+function EditUser() {
+    return(
+        <h1>EditUser</h1>
+    )
+}
+
+export default EditUser
